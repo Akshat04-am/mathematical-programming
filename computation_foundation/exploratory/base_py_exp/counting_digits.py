c3adc561cc12843cd35d938d27cfa2a995ed55a5
Akshat04-am <akshatsachdeva04@gmail.com>
2026-05-16 17:51:24 +0530

    vectors, dot products and Cauchy-Schwarz inequality.

--- computation_foundation/exploratory/base_py_exp/counting_digits.py
+++ computation_foundation/exploratory/base_py_exp/counting_digits.py
@@ -2,10 +2,10 @@ count = 0
 
 for num in range(1, 251):
 
-    # 🔧 Convert number to string
+    #  Convert number to string
     s = str(num)
 
-    # 🔧 Count how many '5' digits inside it
+    #  Count how many '5' digits inside it
     count += s.count("5")
 
 print(count)
